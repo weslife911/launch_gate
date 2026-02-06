0368d66a78c1b67c2e0b8c1441edf1cdcea06348
weslife911 <mfonfubikoma@gmail.com>
2026-02-06 09:04:10 +0100

    made changes to build script and vercel config

--- launch_gate_backend/build_files.sh
+++ launch_gate_backend/build_files.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+
 echo "BUILD START"
-python3.12 -m pip install -r requirements.txt
+
+# Create a virtual environment to avoid "externally-managed-environment" error
+python3.12 -m venv venv
+source venv/bin/activate
+
+# Install requirements within the virtual environment
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run collectstatic
 python3.12 manage.py collectstatic --noinput --clear
+
 echo "BUILD END"
