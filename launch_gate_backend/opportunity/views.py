1452c86e585fb0fd222a42f723065c7cfc989ffe
weslife911 <mfonfubikoma@gmail.com>
2026-03-07 19:54:48 +0100

    added filter by category for opportunities

--- launch_gate_backend/opportunity/views.py
+++ launch_gate_backend/opportunity/views.py
@@ -4,6 +4,12 @@ from .models import Opportunity
 from .serializers import OpportunitySerializer
 
 class OpportunityListView(generics.ListAPIView):
-    queryset = Opportunity.objects.all().order_by('-date_scraped')
     serializer_class = OpportunitySerializer
     permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        queryset = Opportunity.objects.all().order_by('-date_scraped')
+        category = self.request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(category=category)
+        return queryset
