27980a4021b29a733e4dd03092ed17d9d9e00702
weslife911 <mfonfubikoma@gmail.com>
2026-03-09 01:44:32 +0100

    adjusted admin file for opportunity model and updated jazzmin

--- launch_gate_backend/opportunity/admin.py
+++ launch_gate_backend/opportunity/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from opportunity.models import Opportunity
 
-admin.site.register(Opportunity)
+@admin.register(Opportunity)
+class OpportunityAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'date_scraped')
+    list_filter = ('category',)
+    search_fields = ('title', 'description')
+    list_per_page = 20
